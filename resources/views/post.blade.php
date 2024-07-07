a90968c6d3bed7903c426ee678fb796a759b948b
rmjpkl <rmjpkl@gmail.com>
2024-07-07 14:23:09 +0700

    episode 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
   <article class="py-8 max-w-screen-md border-b boder-gray-300">
     <h2 class="mb-1 text-3x1 tracking-tight font-bold text-gray-900">{{ $post['title']}}</h2>
     <div class="text-base text-gray-500">
-      <a href="#">{{  $post['author'] }}</a> | 01 Januari 2024
+      <a href="#">{{  $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light">{{ $post['body'], 150 }} </p>
       <a href="/posts/" class="font-media text-blue-500">Back To Posts More &laquo;</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,7 @@
     <h2 class="mb-1 text-3x1 tracking-tight font-bold text-gray-900">{{ $post['title']}}</h2>
     </a>
     <div class="text-base text-gray-500">
-      <a href="#">{{  $post['author'] }}</a> | 01 Januari 2024
+      <a href="#">{{  $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light">{{  Str::limit($post['body'], 150 )}} </p>
       <a href="/posts/{{  $post['slug'] }}" class="font-media text-blue-500">Read More &raquo;</a>
