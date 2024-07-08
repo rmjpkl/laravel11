67568de4bacd329e8447f743428a4bc580840762
rmjpkl <rmjpkl@gmail.com>
2024-07-08 10:56:48 +0700

    episode 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +28,24 @@ Route::get('/posts/{post:slug}', function (Post $post) {
         ]);
 });
 
+Route::get('/authors/{user:username}', function (User $user) {
+       
+        return view('posts', [
+            'title' => count( $user->posts).' '.'Articles by ' . $user->name,
+            'posts' => $user->posts
+        ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+       
+        return view('posts', [
+            'title' => 'Articles In Catagory ' . $category->name,
+            'posts' => $category->posts
+        ]);
+});
+
+
+
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
@@ -33,3 +53,5 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
+
+
